9d045af7cd88cfa9efe9d2893ea2d152c7edee77
Usmonov12345 <toxirusmonov04@gmail.com>
2022-09-23 17:19:55 +0500

    7-dars tugadi

---
+++ style/main.js
@@ -0,0 +1,47 @@
+// // //zornarsa
+// const colors = ["red", "green", "blue","#999","#f1f"]
+
+// const random = colors[Math.floor(Math.random() * colors.length)]
+// const body = document.body.style.backgroundColor = random;
+//zor narsa
+
+// const elements = document.querySelectorAll(". js-target");
+
+// for (let i =0; i<elements.length; i++){
+//     const currentElement = elements[i];
+//     currentElement.textContent = "hello by all li tags modified"
+// }
+
+// function clickMe(){
+//     const colors = ["#999"]
+
+// const random = colors[Math.floor(Math.random() * colors.length)]
+// const body = document.body.style.backgroundColor = random;
+// }1
+
+// const btn = document.querySelector(".btn");
+// btn.addEventListener('click',function () {
+//     console.log("hello from ");
+//     document.body.style.color =  "#999"
+// }
+// )
+
+// const input =  document.querySelector("#value")
+// const parag =  document.querySelector(".parag")
+
+// input.addEventListener("keyup", function (){
+//     parag.textContent = input.value;
+// })
+
+const piyoda =  document.querySelector("#piyoda")
+
+const viloseped =  document.querySelector(" #viloseped")
+
+const mawida =  document.querySelector("#mawida")
+
+const samalyod =  document.querySelector("#samalyod")
+
+
+piyoda.addEventListener("keyup", function{
+    console.log(piyoda);
+})
